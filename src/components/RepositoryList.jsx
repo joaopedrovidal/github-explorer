f42b64f489102c1fce23021412f0559d52b79cf2
joaopedrovidal <ppvidalsantos@gmail.com>
2025-06-20 11:45:27 -0300

    Aula 12 -  Primeiro componente React

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,35 @@
+const repositoryName = 'unform2'
+
+export function RepositoryList() {
+    return (
+        <section>
+            <h1>Lista de repositórios</h1>
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositórios
+                    </a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositórios
+                    </a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositórios
+                    </a>
+                </li>
+            </ul>
+        </section>
+    )
+}
